d3fcdbf5179926a04e0b0c2b3067a479f8869eb9
Jere Sumell <34176980+jjsume@users.noreply.github.com>
2021-04-21 18:11:51 +0300

    Add files via upload

--- QueueandStack.py
+++ QueueandStack.py
@@ -9,12 +9,15 @@ class Jono:
 
     def lisaa(self,tieto):
         #Lisätään alkio oikeaan kohtaan
+        print("Lisätään jonoon luku " +str(tieto))    
         self.lista.append(tieto)
+        
 
     def poistaSeuraava(self):
         # palauttaa jonon seuraavan alkion ja  poistaa sen listasta.
         t = self.lista[0]
         self.lista.pop(0)
+        print ("" +" Jonosta poistetaan " +str(t))
         return t
 
 
@@ -32,14 +35,16 @@ class Pino:
 
     def lisaa(self,tieto):
         #Lisätään alkio oikeaan kohtaan
-        self.lista.append(tieto)
+        print("Lisätään Pinoon " +str(tieto))
+
+        self.lista.insert(len(self.lista)-1,tieto)
 
     def poistaSeuraava(self):
         # palauttaa jonon seuraavan alkion ja  poistaa sen listasta.
         t = self.lista[len(self.lista)-1]
         self.lista.pop(len(self.lista)-1)
+        print("Pinosta poistetaan " +str(t))
         return t
-
 
     def tulosta(self):
         #Tulostetaan
@@ -50,8 +55,8 @@ jono = Jono()
 
 for x in range(5):
     luku = random.randrange(0,100)
-    print(luku)
     jono.lisaa(luku)
+    jono.tulosta()
 
 while (len(jono.lista)) > 1:
     jono.tulosta()
@@ -64,6 +69,7 @@ for y in range(5):
     luku = random.randrange(0,100)
     print(luku)
     pino.lisaa(luku)
+    pino.tulosta()
 
 while (len(pino.lista)) > 1:
     pino.tulosta()
